91080d79be7acb4d6b4e61779f01c14758f09261
[arkimbauer] <[adria.arki@gmail.com]>
2019-11-07 16:18:01 +0100

    refactor: implementado First Class collection Neighbors y override en cell position y neighbors para poder comparar entre ellos

---
+++ GameOfLifeV2/GameOfLifeV2/CellPositionShould.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GameOfLifeV2
+{
+    public class CellPositionShould
+    {
+        [Fact]
+        public void ReturnNeighborsOfTheCell()
+        {
+            CellPosition cellPosition = new CellPosition(0,0);
+
+            var result = cellPosition.GetNeighbors();
+
+            Assert.Equal(new Neighbors(new List<CellPosition>{ new CellPosition(1, 0), new CellPosition(-1,0), new CellPosition(0, 1), new CellPosition(0, -1), new CellPosition(1, 1), new CellPosition(-1, -1), new CellPosition(1,-1), new CellPosition(-1,1) }), result);
+        }
+    }
+}
