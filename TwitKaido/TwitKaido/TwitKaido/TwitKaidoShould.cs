dce76786e5c42e551e8c49bd552667d77bb00c13
[arkimbauer] <[adria.arki@gmail.com]>
2019-11-14 17:12:27 +0100

    test: ReturnAllMessagesAfterIntroduceMessagesOnAList

--- TwitKaido/TwitKaido/TwitKaido/TwitKaidoShould.cs
+++ TwitKaido/TwitKaido/TwitKaido/TwitKaidoShould.cs
@@ -20,5 +20,21 @@ namespace TwitKaido
 
             Assert.Equal(new List<string>{ "Holi", "How are you" }, result);   
         }
+
+        [Fact]
+        public void ReturnAllMessagesAfterIntroduceMessagesOnAList()
+        {
+            var firstPost = "Adria -> Holi";
+            var secondPost = "Adria -> How are you";
+            var thirdPost = "Adria -> Where are you from?";
+
+            List<string> posts = new List<string> { firstPost, secondPost, thirdPost };
+
+            TwitKaido twitKaido = new TwitKaido(posts);
+
+            var result = twitKaido.ReturnPosts();
+
+            Assert.Equal(new List<string> { "Holi", "How are you", "Where are you from?"}, result);
+        }
     }
 }
