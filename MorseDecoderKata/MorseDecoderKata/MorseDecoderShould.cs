ed76e4c2ff04a14261771cc39bf766bb019253bf
[arkimbauer] <[adria.arki@gmail.com]>
2019-11-26 15:10:03 +0100

    added double etst Kata

---
+++ MorseDecoderKata/MorseDecoderKata/MorseDecoderShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MorseDecoderKata
+{
+    public class MorseDecoderShould 
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
