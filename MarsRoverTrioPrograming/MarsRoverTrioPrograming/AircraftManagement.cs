6b747cef565fb3d9bd253436e10c627b8fdb2243
malar <adria.arki@gmail.com>
2019-12-09 22:53:05 +0100

    refact: add dictionary

--- MarsRoverTrioPrograming/MarsRoverTrioPrograming/AircraftManagement.cs
+++ MarsRoverTrioPrograming/MarsRoverTrioPrograming/AircraftManagement.cs
@@ -28,17 +28,17 @@ namespace MarsRoverTrioPrograming
                 {Compass.SW, Compass.NE},
             };
 
-            _navigateToDictionary = new Dictionary<Compass, Action>
-            {
-                {Compass.N, _axis.MoveNorth},
-                {Compass.NE, _axis.MoveNorthEast},
-                {Compass.E, _axis.MoveEast},
-                {Compass.SE, _axis.MoveSouthEast},
-                {Compass.S, _axis.MoveSouth},
-                {Compass.SW, _axis.MoveSouthWest},
-                {Compass.W, _axis.MoveWest},
-                {Compass.NW, _axis.MoveNorthWest}
-            };
+            //_navigateToDictionary = new Dictionary<Compass, Action>
+            //{
+            //    {Compass.N, _axis.MoveNorth},
+            //    {Compass.NE, _axis.MoveNorthEast},
+            //    {Compass.E, _axis.MoveEast},
+            //    {Compass.SE, _axis.MoveSouthEast},
+            //    {Compass.S, _axis.MoveSouth},
+            //    {Compass.SW, _axis.MoveSouthWest},
+            //    {Compass.W, _axis.MoveWest},
+            //    {Compass.NW, _axis.MoveNorthWest}
+            //};
 
             _fuel = fuel;    
             _axis = axis;
@@ -54,59 +54,53 @@ namespace MarsRoverTrioPrograming
                 if (compass == Compass.N)
                 {
                     _axis.MoveNorth();
-
                 }
 
                 if (compass == Compass.NE)
                 {
                     _axis.MoveNorthEast();
-
                 }
 
                 if (compass == Compass.E)
                 {
                     _axis.MoveEast();
-
                 }
 
                 if (compass == Compass.SE)
                 {
                     _axis.MoveSouthEast();
-
                 }
 
                 if (compass == Compass.S)
                 {
                     _axis.MoveSouth();
-
                 }
 
                 if (compass == Compass.SW)
                 {
                     _axis.MoveSouthWest();
-
                 }
 
                 if (compass == Compass.W)
                 {
                     _axis.MoveWest();
-
                 }
 
                 if (compass == Compass.NW)
                 {
                     _axis.MoveNorthWest();
-
                 }
+                _fuel--;
             }
-            
+
+
             if (_fuel == Empty)
             {
                 return;
             }
-
-            _fuel --;
         }
+
+
         private void CalculateFuelToReturnHome(int fuel)
         {
             _fuelToReturnHome = _fuel / 2 + fuel % 2;
