5ce26931ca456f0801762c8275869face4ca8032
Adrià Arquimbau <adria.arki@gmail.com>
2019-12-09 17:48:20 +0100

    feat: added dictionary falta implementar no avançi mes

--- MarsRoverTrioPrograming/MarsRoverTrioPrograming/AircraftManagement.cs
+++ MarsRoverTrioPrograming/MarsRoverTrioPrograming/AircraftManagement.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MarsRoverTrioPrograming
 {
@@ -9,71 +10,93 @@ namespace MarsRoverTrioPrograming
         private readonly Axis _axis;
         private readonly List<Compass> _wayBack = new List<Compass>();
         private int _highFuel;
+        private readonly Dictionary<Compass,Compass> GoBackCompass ;
+        const int empty = 0;
         
         public AircraftManagement(int fuel, Axis axis)
         {
+            GoBackCompass = new Dictionary<Compass, Compass>
+            {
+                {Compass.N, Compass.S},
+                {Compass.S, Compass.N},
+                {Compass.W, Compass.E},
+                {Compass.E, Compass.W},
+                {Compass.NE, Compass.SW},
+                {Compass.NW, Compass.SE},
+                {Compass.SE, Compass.NW},
+                {Compass.SW, Compass.NE},
+            };
+            //dictionary per methods
             _fuel = fuel;    
             _axis = axis;
-            GetHighFuel(fuel);
+            CalculateHalfOfTheFuelRounded(fuel);
         }
 
         public void NavigateTo(Compass compass)
         {
             if (_fuel > _highFuel)
             {
-                if (compass == Compass.N )
-                {
-                    _axis.MoveNorth();
-                    _fuel --;
-                }
-            
-                if (compass == Compass.NE )
-                {
-                    _axis.MoveNorthEast();
-                    _fuel --;
-                }
-
-                if (compass == Compass.E )
-                {
-                    _axis.MoveEast();
-                    _fuel --;
-                }
-            
-                if (compass == Compass.SE )
-                {
-                    _axis.MoveSouthEast();
-                    _fuel --;
-                }
-            
-                if (compass == Compass.S )
-                {
-                    _axis.MoveSouth();
-                    _fuel --;
-                }
-
-                if (compass == Compass.SW )
-                {
-                    _axis.MoveSouthWest();
-                    _fuel --;
-                }
-            
-                if (compass == Compass.W)
-                {
-                    _axis.MoveWest();
-                    _fuel --;
-                }
-            
-                if (compass == Compass.NW )
-                {
-                    _axis.MoveNorthWest();
-                    _fuel --;
-                }
+                _wayBack.Add(GoBackCompass[compass]);
             }
+            
+            if (_fuel == empty)
+            {
+                return;
+            }
+
+            if (compass == Compass.N )
+            {
+                _axis.MoveNorth();
+                
+            }
+        
+            if (compass == Compass.NE )
+            {
+                _axis.MoveNorthEast();
+                
+            }
+
+            if (compass == Compass.E )
+            {
+                _axis.MoveEast();
+               
+            }
+        
+            if (compass == Compass.SE )
+            {
+                _axis.MoveSouthEast();
+                
+            }
+        
+            if (compass == Compass.S )
+            {
+                _axis.MoveSouth();
+                
+            }
+
+            if (compass == Compass.SW )
+            {
+                _axis.MoveSouthWest();
+                
+            }
+        
+            if (compass == Compass.W)
+            {
+                _axis.MoveWest();
+               
+            }
+        
+            if (compass == Compass.NW )
+            {
+                _axis.MoveNorthWest();
+                
+            }
+            _fuel --;
         }
-        private void GetHighFuel(int fuel)
+        private void CalculateHalfOfTheFuelRounded(int fuel)
         {
             _highFuel = _fuel / 2 + fuel % 2;
-        }
+        }    
         
     }
 }
