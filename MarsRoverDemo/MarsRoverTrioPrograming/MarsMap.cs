cce29bd6c0fffe2548036cbb0b011b125d82b4ee
[arkimbauer] <[adria.arki@gmail.com]>
2019-12-10 12:50:54 +0100

    refact: only one dictionary with movements and type of axis

--- MarsRoverDemo/MarsRoverTrioPrograming/MarsMap.cs
+++ MarsRoverDemo/MarsRoverTrioPrograming/MarsMap.cs
@@ -9,7 +9,17 @@ namespace MarsRoverDemo
         private readonly int _width;
         private readonly int _height;
         private readonly List<Axis> _gridPositions = new List<Axis>();
-        private Dictionary<Compass, Action> _navigateTo;
+        private Dictionary<Compass, Action<Axis>> _navigateTo = new Dictionary<Compass, Action<Axis>>
+        {
+            {Compass.N, (axisToChange)=> { axisToChange.MoveNorth(); }},
+            {Compass.NE, (axisToChange)=> { axisToChange.MoveNorthEast(); }},
+            {Compass.E, (axisToChange)=> { axisToChange.MoveEast(); }},
+            {Compass.SE, (axisToChange)=> { axisToChange.MoveSouthEast(); }},
+            {Compass.S, (axisToChange)=> { axisToChange.MoveSouth(); }},
+            {Compass.SW, (axisToChange)=> { axisToChange.MoveSouthWest(); }},
+            {Compass.W, (axisToChange)=> { axisToChange.MoveWest(); }},
+            {Compass.NW, (axisToChange)=> { axisToChange.MoveNorthWest(); }},
+        };
         private Dictionary<Compass, Action> _simulateNavigateTo;
 
         public MarsMap()
@@ -22,72 +32,13 @@ namespace MarsRoverDemo
         public void NavigateTo(Compass compass, Axis axis)
         {
             var cloneAxis = axis.CloneAxis();
-            _navigateTo = new Dictionary<Compass, Action>
-            {
-                {Compass.N, axis.MoveNorth},
-                {Compass.NE, axis.MoveNorthEast},       
-                {Compass.E, axis.MoveEast},
-                {Compass.SE, axis.MoveSouthEast},
-                {Compass.S, axis.MoveSouth},
-                {Compass.SW, axis.MoveSouthWest},
-                {Compass.W, axis.MoveWest},
-                {Compass.NW, axis.MoveNorthWest},
-            };
-            _simulateNavigateTo = new Dictionary<Compass, Action>
-            {
-                {Compass.N, cloneAxis.MoveNorth},
-                {Compass.NE, cloneAxis.MoveNorthEast},
-                {Compass.E, cloneAxis.MoveEast},
-                {Compass.SE, cloneAxis.MoveSouthEast},
-                {Compass.S, cloneAxis.MoveSouth},
-                {Compass.SW, cloneAxis.MoveSouthWest},
-                {Compass.W, cloneAxis.MoveWest},
-                {Compass.NW, cloneAxis.MoveNorthWest},
-            };
 
-            _simulateNavigateTo[compass]();
+            _navigateTo[compass](cloneAxis);
 
-            if (_gridPositions.Contains(new Axis(cloneAxis.PositionY, cloneAxis.PositionX)))
+            if (_gridPositions.Contains(cloneAxis))
             {
-                _navigateTo[compass]();
+                _navigateTo[compass](axis);
             }
-            
-            
-            //if (compass == Compass.NE && axis.PositionY < _height && axis.PositionX < _width)
-            //{
-            //    axis.MoveNorthEast();
-            //}
-
-            //if (compass == Compass.E && axis.PositionX < _width)
-            //{
-            //    axis.MoveEast();
-            //}
-            
-            //if (compass == Compass.SE && axis.PositionY > downLeftLimitPosition && axis.PositionX < _width)
-            //{
-            //    axis.MoveSouthEast();
-            //}
-            
-            //if (compass == Compass.S && axis.PositionY > downLeftLimitPosition)
-            //{
-            //    axis.MoveSouth();
-            //}
-
-            //if (compass == Compass.SW && axis.PositionY > downLeftLimitPosition &&
-            //    axis.PositionX > downLeftLimitPosition)
-            //{
-            //    axis.MoveSouthWest();
-            //}
-            
-            //if (compass == Compass.W && axis.PositionX > downLeftLimitPosition)
-            //{
-            //    axis.MoveWest();
-            //}
-            
-            //if (compass == Compass.NW && axis.PositionY < _height && axis.PositionX > downLeftLimitPosition)
-            //{
-            //   axis.MoveNorthWest();
-            //}
         }
 
         private void GenerateGridPositionsList(int width, int height)
